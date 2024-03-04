42806e9ae8c2b85a5b5c3e5e13a59c56fbaecfa8
John Jeffers <john@jeffers.cc>
2024-03-03 21:08:33 -0700

    fix ohmyzsh

--- setup/modules/ohmyzsh.sh
+++ setup/modules/ohmyzsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function setup_ohmyzsh() {
-  if dir_missing "${HOME}/oh-my-zsh"; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  if dir_missing "${HOME}/.oh-my-zsh"; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null
   fi
 }
