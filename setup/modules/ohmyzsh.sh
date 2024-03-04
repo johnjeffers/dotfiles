590eb9aaed6938fb30c1730fe282b52755dee416
John Jeffers <john@jeffers.cc>
2024-03-03 21:19:41 -0700

    omz success msg

--- setup/modules/ohmyzsh.sh
+++ setup/modules/ohmyzsh.sh
@@ -4,4 +4,6 @@ function setup_ohmyzsh() {
   if dir_missing "${HOME}/.oh-my-zsh"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null
   fi
+
+  success "Configured oh-my-zsh"
 }
