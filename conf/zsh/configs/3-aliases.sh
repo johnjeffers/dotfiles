52f494c53ffba79eac5a081d1697ef0a9bae65b2
John Jeffers <john@jeffers.cc>
2024-06-04 11:55:54 -0600

    clean up aliases

--- conf/zsh/configs/3-aliases.sh
+++ conf/zsh/configs/3-aliases.sh
@@ -3,15 +3,17 @@
 
 ### Aliases
 
-alias c="clear"
-alias swup="~/git/personal/dotfiles/setup.sh"
-alias tf="terraform"
-alias tfclean="rm .terraform.lock.hcl && rm -rf .terraform"
-alias watch="watch "
+alias c='clear'
+alias swup='~/git/personal/dotfiles/setup.sh'
+alias tf='terraform'
+alias tfclean='rm .terraform.lock.hcl && rm -rf .terraform'
+alias watch='watch '
 
 # k8s aliases
-alias kgn="kubectl get nodes -L node-group-name -L topology.kubernetes.io/zone | sort -k6"
-alias kns="kubens"
+alias kgn='kubectl get nodes -L node-group-name -L topology.kubernetes.io/zone | sort -k6'
+alias kns='kubens'
+alias wkgn='watch "kubectl get nodes -L node-group-name -L topology.kubernetes.io/zone | sort -k6"'
+alias wkgpa='watch "kubectl get po --all-namespaces | grep -v '\''Completed'\'' | grep -vE '\''1/1|2/2|3/3|4/4|5/5|6/6'\''"'
 
 # AWS aliases
 alias ssologin='aws sso login --profile ${SSO_PROFILE:-fusionauth-dev}'
